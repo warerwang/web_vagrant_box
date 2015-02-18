a4c21cfa295f28e73abd1e4c9561179a28334825
warerwang <warerwang@126.com>
2015-02-19 00:36:59 +0800

    update vagrant file.

--- shell/config.sh
+++ shell/config.sh
@@ -2,3 +2,4 @@
 cp /vagrant/config/nginx.conf /etc/nginx/nginx.conf
 cp /vagrant/config/php-fpm.conf /etc/php-fpm.conf
 cp /vagrant/config/php.ini /etc/php.ini
+cp /vagrant/config/my.cnf /etc/my.cnf
