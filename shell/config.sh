2aa6d5bd8671f6cf4bb6c9d7e5d022082ed1fee3
warerwang <warerwang@126.com>
2015-02-21 00:10:29 +0800

    provision config.

--- shell/config.sh
+++ shell/config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cp /vagrant/config/nginx.conf /etc/nginx/nginx.conf
 cp /vagrant/config/php-fpm.conf /etc/php-fpm.conf
+cp /vagrant/config/php-fpm.d/www.conf /etc/php-fpm.d/www.conf
 cp /vagrant/config/php.ini /etc/php.ini
 cp /vagrant/config/my.cnf /etc/my.cnf
